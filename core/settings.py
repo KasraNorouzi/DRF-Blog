6fa4d6a848a934af312c80793bafa01211f93e74
Kasra Norouzi <kasranorouzi95@gmail.com>
2024-06-19 12:23:39 +0330

    setting up rest framework

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "accounts.apps.AccountsConfig",
-    "blog.apps.BlogConfig"
+    "blog.apps.BlogConfig",
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
