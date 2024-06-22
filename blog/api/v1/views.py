9f457f82392fbb14d770ffc5b4d8a44247d63ddb
Kasra Norouzi <kasranorouzi95@gmail.com>
2024-06-22 10:01:32 +0330

    adde filter backends and permissions

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -2,15 +2,21 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework import viewsets
 from .serializers import PostSerializer, CategorySerializer
 from blog.models import Post, Category
+from .permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostModelViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category', 'author')
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+
