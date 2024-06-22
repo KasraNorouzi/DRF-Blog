208d73835ba9aa0ca160cb9eb945e6a66215ea34
Kasra Norouzi <kasranorouzi95@gmail.com>
2024-06-22 10:11:27 +0330

    updating filters

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -4,14 +4,17 @@ from .serializers import PostSerializer, CategorySerializer
 from blog.models import Post, Category
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
     filterset_fields = ('category', 'author')
+    search_fields = ('title', 'content')
+    ordering_fields = ['published_date']
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
