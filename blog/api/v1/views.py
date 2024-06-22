6ab701e60f60373bfb1cda44872f226c2b29934f
Kasra Norouzi <kasranorouzi95@gmail.com>
2024-06-22 10:43:59 +0330

    updating filters and adding pagination

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,10 +1,12 @@
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import viewsets
 from .serializers import PostSerializer, CategorySerializer
 from blog.models import Post, Category
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from .paginations import DefaultPagination
+from .filters import PostFilters
 
 
 class PostModelViewSet(viewsets.ModelViewSet):
@@ -12,14 +14,13 @@ class PostModelViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
-    filterset_fields = ('category', 'author')
+    filterset_class = PostFilters
     search_fields = ('title', 'content')
     ordering_fields = ['published_date']
+    pagination_class = DefaultPagination
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
-
-
